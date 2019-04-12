e01c96407147018126767265dcf8f9610a33b39a
Benjamin-Vandersmissen <benjamin.vandersmissen@student.uantwerpen.be>
2019-04-12 16:04:14 +0200

    added a testfile for some of the warnings associated with typechecking

---
+++ warnings.c
@@ -0,0 +1,24 @@
+int f(float* a){
+
+}
+
+int main(){
+    int a;
+    float b;
+    a = b;
+
+    int* c;
+    int ** d;
+    int* e[2];
+    float* f;
+
+    c = d;
+    d = c;
+    d = e;
+    c = a;
+    f = c;
+
+    e[0] = e;
+
+    f(c);
+}
