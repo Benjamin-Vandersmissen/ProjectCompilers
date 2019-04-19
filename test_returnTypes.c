fff947fdb7630277498daf67bb224ee818c0b4b3
Benjamin-Vandersmissen <Benjamin.Vandersmissen@student.uantwerpen.be>
2019-04-19 23:15:25 +0200

    Added some tests

---
+++ test_returnTypes.c
@@ -0,0 +1,49 @@
+// test typechecking for return statements
+
+
+// should warn
+char f1(){
+    return 1;
+}
+
+// should warn
+int f2(){
+    return 1.0;
+}
+
+// should warn
+int f3(){
+    int* a;
+    return a;
+}
+
+// should warn
+int* f4(){
+    return 0;
+}
+
+// should warn
+int* f5(){
+    float* a;
+    return a;
+}
+
+// should throw error
+int* f6(){
+    return 1.0;
+}
+
+//should throw error
+float f7(){
+    int* a;
+    return a;
+}
+
+//should throw error
+void f8(){
+    return 1;
+}
+
+int f9(){
+    return;
+}
---
+++ test_unreachableCode.c
@@ -0,0 +1,14 @@
+// test not generating useless code after return statement
+
+int test(){
+    int a = 0;
+    while(a){
+        int b = 0;
+        return 0;
+        b = b + 1;
+    }
+    return 1;
+    int b = 0;
+    int c = 0;
+    b = c * 20 + b;
+}
