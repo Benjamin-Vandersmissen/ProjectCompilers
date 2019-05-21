8e716e47415c0a3ed10cdf5a50bdc3c1acf64c2d
Laurens Van Damme <laurensvandamme@hotmail.com>
2019-05-21 16:39:20 +0200

    Nearly bugless get registers function

--- ExpressionTree.py
+++ ExpressionTree.py
@@ -1,28 +1,25 @@
 class ExpressionTree:
-    def __init__(self, variable, left, right, operator):
+    def __init__(self, variable, left, right, parent = None):
         self.var = variable
         self.ershovNumber = None
         self.register = None
-        if left is not None and right is not None:
-            self.left = ExpressionTree(left, None, None, None)
-            self.right = ExpressionTree(right, None, None, None)
-            self.operator = operator
-        else:
-            self.left = None
-            self.right = None
 
-    def addNode(self, var, left, right, operator):
+        if left is None or right is None:
+            self.left = ExpressionTree(left, None, None, self)
+            self.right = ExpressionTree(right, None, None, self)
+
+    def addNode(self, var, left, right):
         if self.left is None or self.right is None:
             return False
         if self.left.var == var:
-            self.left = ExpressionTree(var, left, right, operator)
+            self.left = ExpressionTree(var, left, right, self)
             return True
         elif self.right.var == var:
-            self.right = ExpressionTree(var, left, right, operator)
+            self.right = ExpressionTree(var, left, right, self)
             return True
-        elif self.left.addNode(var, left, right, operator):
+        elif self.left.addNode(var, left, right):
             return True
-        elif self.right.addNode(var, left, right, operator):
+        elif self.right.addNode(var, left, right):
             return True
         else:
             return False
@@ -46,11 +43,24 @@ class ExpressionTree:
             registerMap = dict()
         if self.left is None or self.right is None:
             return registerMap
+
         if self.left.ershovNumber == self.right.ershovNumber:
-            registerMap = self.right.getRegisters(baseNumber + 1, registerMap)
+            if self.left.left is None or self.left.right is None:
+                self.left.register = baseNumber + self.ershovNumber - 1
+                registerMap[self.left.var] = self.left.register
+                self.right.register = baseNumber + self.ershovNumber
+                registerMap[self.right.var] = self.right.register
+            else:
+                registerMap = self.right.getRegisters(baseNumber + 1, registerMap)
             self.register = baseNumber + self.ershovNumber
-        else:
-            registerMap = self.right.getRegisters(baseNumber, registerMap)
+        else: # TODO: depending which is the biggest!! TO BE FIXED
+            if self.right.left is None or self.right.right is None:
+                self.left.register = baseNumber + self.ershovNumber - 1  # if big
+                registerMap[self.left.var] = self.left.register
+                self.right.register = baseNumber + self.ershovNumber - 2  # if small
+                registerMap[self.right.var] = self.right.register
+            else:
+                registerMap = self.right.getRegisters(baseNumber, registerMap)
             self.register = baseNumber + self.ershovNumber - 1
         self.left.getRegisters(baseNumber, registerMap)
         registerMap[self.var] = self.register
