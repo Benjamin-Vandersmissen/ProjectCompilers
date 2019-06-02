f5191fd5b522c843ccf10161b4e36f8efca86d7c
Laurens Van Damme <laurensvandamme@hotmail.com>
2019-06-02 20:21:54 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,17 +2,18 @@
 java -jar antlr-4.7.2-complete.jar -Dlanguage=Python3 smallC.g4 -visitor
 for filename in tests/*.c
 do
-    echo "\n\nStarting: $filename \n"
+    printf "\n\n(bash) Starting: $filename \n"
 
-    if ["$filename" = "test_llvm.c"]
-    then
+    if [ "$filename" == "tests/test_llvm.c" ]; then
         python3 c2llvm.py "$filename"
-    elif ["$filename" = "test_mips.c"]
-    then
+    elif [ "$filename" == "tests/test_mips.c" ]; then
         python3 c2mips.py "$filename"
     else
         python3 c2mips.py "$filename" true
     fi
 
+    printf "(bash) Making image of dot...\n"
     dot -Tpng "${filename%.*}.dot" -o "${filename%.*}.png"
+
+    printf "(bash) End $filename"
 done
